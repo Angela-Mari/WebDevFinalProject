ebfcf347704a23c904c864dc768ef1772188652c
Angela George <ageorge5@zagmail.gonzaga.edu>
2020-11-16 17:48:38 -0600

    set up react app

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className = "container">
+      <header>
+        header
+        <nav>menu</nav>
+      </header>
+      
+      <div>inpirational quote</div>
+      <body>the entries</body>
+      <h1>Hello Daily Diary Hub</h1>
+    </div>
+  );
+}
+
+export default App;
