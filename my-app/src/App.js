ead327cf50deeac5bb2cc5b18fcdd24f6a308189
Angela George <ageorge5@zagmail.gonzaga.edu>
2020-11-16 21:00:07 -0600

    basic layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
-
+<script>
+     $myVar = "hi"; 
+</script>
 
 function App() {
   return (
-    <div className = "container">
-      <header>
-        header
-        <nav>menu</nav>
-      </header>
+    
+    <div class = "container">
       
-      <div>inpirational quote</div>
-      <body>the entries</body>
-      <h1>Hello Daily Diary Hub</h1>
+      <header>
+      <h1>Daily Diary Hub</h1>
+        <nav>
+          <p><button>log out</button></p>
+          <p><button>change theme</button></p>
+        </nav>
+      </header>
+
+      <div class = "special">
+        <p>this is the daily quote container</p>
+        <div class = "quote">
+          <button class = "back_btn, nav_btn">next</button>
+          <p class = "qt_txt">inpirational quote</p>
+          <button class = "forward_btn, nav_btn">next</button>
+        </div>
+      </div>
+      
+      <body>
+        <div class = "controls">
+          <button class = "write_btn">write</button>
+        </div>
+        
+        the entries
+      </body>
+      
+      <footer>
+        footer content
+      </footer>
     </div>
   );
 }
