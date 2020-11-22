1f53a2df19626ae04b1272ce005de512d41550ec
Angela George <ageorge5@zagmail.gonzaga.edu>
2020-11-22 16:24:20 -0600

    forward and back bttns work

--- front-end-react/src/components/QuoteContainer.js
+++ front-end-react/src/components/QuoteContainer.js
@@ -17,18 +17,69 @@ const quotesInner = {
     padding: 20
 }
 
-function QuoteContainer(){
-    return(
-        <div style = {displayQuotes}>
-            <button>back</button>
-            <div style = {quotesInner}>
-                <h3>{myQuotes[0].quote}</h3>
-                <p>-{myQuotes[0].author}</p>
-                <button>+</button>
+
+
+class QuoteContainer extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            index: 0,
+            length: myQuotes.length -1 
+        }
+        this.backupIndex = this.backupIndex.bind(this)
+        this.forwardIndex = this.forwardIndex.bind(this)
+    }
+
+    backupIndex(){
+        this.setState(prevState => {
+            
+            if (prevState.index -1 < 0){
+                return {
+                    index : prevState.length,
+                    length : prevState.length
+                }
+            }
+            else {
+                return {
+                    index : prevState.index - 1,
+                    length : prevState.length
+                }
+            } 
+        })
+    }
+
+    forwardIndex(){
+        this.setState(prevState => {
+            
+            if (prevState.index +1 > prevState.length){
+                return {
+                    index : 0,
+                    length : prevState.length
+                }
+            }
+            else {
+                return {
+                    index : prevState.index + 1,
+                    length : prevState.length
+                }
+            } 
+        })
+    }
+
+    render() {
+
+        return(
+            <div style = {displayQuotes}>
+                <button onClick = {this.backupIndex}>back</button>
+                <div style = {quotesInner}>
+                    <h3>{myQuotes[this.state.index].quote}</h3>
+                    <p>-{myQuotes[this.state.index].author}</p>
+                    <button>+</button>
+                </div>
+                <button onClick = {this.forwardIndex}>forward</button>
             </div>
-            <button>forward</button>
-        </div>
-    )
+        )
+    }  
 }
 
 export default QuoteContainer
