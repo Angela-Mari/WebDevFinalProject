f9d7f90ad412afcef405f6f2009059e4c9a7a613
Angela George <ageorge5@zagmail.gonzaga.edu>
2020-11-22 22:52:15 -0600

    add entry done

--- front-end-react/src/components/DiaryCards.js
+++ front-end-react/src/components/DiaryCards.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import DiaryEntry from './DiaryEntry'
-import Entries from './Entries'
 
-function DiaryCards() {
+class DiaryCards extends React.Component {
 
-    const myGridStyle = {
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-        gap: 10,
+    constructor(props){
+        super()
+        this.state = {
+            entries : props.entries
+            
+        }
+        //console.log(this.state.entries)
     }
 
-    const entriesArray = Entries.map(item => <DiaryEntry entry = {item}/>)
-    return (
-        <div style = {myGridStyle}>
-            {entriesArray}
-        </div>
-    )
-    
+    render(){
+        const myGridStyle = {
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gap: 10,
+        }
+
+        const entriesArray = this.state.entries.map(item => <DiaryEntry entry = {item}/>)
+        return (
+            <div style = {myGridStyle}>
+                {entriesArray}
+            </div>
+        )
+    }
 }
 
 export default DiaryCards
